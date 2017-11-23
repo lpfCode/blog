12eee2175b3dcb92e2278692ef1a5c1843e86c40
lpfCode <lpf_student@163.com>
2017-11-23 16:22:30 +0800

    tjcxxs

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -63,6 +63,7 @@ class StudentController extends Controller
         $param = $request->input('key');
         $value = $request->input('value');
         $result = StudentService::getInstance()->selectByParam($param,$value);
+        dd($result);
         return View::make('students.show')->with('studentInfo',$result);
 //        return View::make(students.show)->with('studentInfo',$result)->with('key',$param)->with('value',$value);
     }
