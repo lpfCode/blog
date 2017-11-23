40b2ccfcb8b8444df290cc76e2dd87b1cbf352fb
lpfCode <lpf_student@163.com>
2017-11-23 10:20:32 +0800

    引入View包错误修改,第一参数加了引号,修改了edit

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -15,29 +15,35 @@
             <li><a href="{{ URL::to('st/create') }}">添加</a>
         </ul>
     </nav>
-    <h1>编辑: {{ $blog->name }}</h1>
+    <h1>编辑: {{ $studentInfo->name }}</h1>
     <table class="table table-striped table-bordered">
         <form method="post" action="/blog/update">
             {{csrf_field()}}
             <tr>
-                <td>用户名称：</td>
-                <input style="display: none" type="text" name="id" value="{{ $blog->id }}">
-                <td><input type="text" name="name" value="{{ $blog->name }}"></td>
+                <td>姓名：</td>
+                <input style="display: none" type="text" name="id" value="{{ $studentInfo->id }}">
+                <td><input type="text" name="name" value="{{ $studentInfo->name }}"></td>
             </tr>
             <tr>
-                <td>用户邮箱：</td>
-                <td><input type="text" name="email" value="{{ $blog->email }}"></td>
+                <td>年龄：</td>
+                <td><input type="text" name="age" value="{{ $studentInfo->age }}"></td>
             </tr>
             <tr>
-                <td>博客等级：</td>
+                <td>科目：</td>
                 <td>
-                    <select name="blog_level">
+                    <select name="obj">
                         {{--  有@标识符之后，就会用PHP解释器解析@if和@endif之间的代码就不用加{{}}  --}}
-                        <option value="1" @if($blog->blog_level ==1) selected @endif>1</option>
-                        <option value="2" @if($blog->blog_level ==2) selected @endif>2</option>
-                        <option value="3" @if($blog->blog_level ==3) selected @endif>3</option>
+                        <option value="语文" @if($studentInfo->obj =='语文') selected @endif>语文</option>
+                        <option value="数学" @if($studentInfo->obj =='数学') selected @endif>数学</option>
+                        <option value="英语" @if($studentInfo->obj =='英语') selected @endif>英语</option>
+                        <option value="计算机组成原理" @if($studentInfo->obj =='计算机组成原理') selected @endif>计算机组成原理</option>
+                        <option value="计算机网络" @if($studentInfo->obj =='计算机网络') selected @endif>计算机网络</option>
                     </select>
                 </td>
+            </tr>
+            <tr>
+                <td>成绩：</td>
+                <td><input type="text" name="score" value="{{$studentInfo->score}}"></td>
             </tr>
             <tr>
                 <td colspan="2">
