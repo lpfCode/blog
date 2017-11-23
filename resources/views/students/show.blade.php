6960fad670bc9f9a6bef34a0bdb6ebd7e5753aca
lpfCode <lpf_student@163.com>
2017-11-23 19:34:22 +0800

    tjcqqwaaaapa

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -56,7 +56,7 @@
                     <td>操作</td>
                 </tr>
             </thead>
-            {{--{{$studentInfo}}--}}
+            {{$studentInfo}}
             <tbody>
                 @foreach($studentInfo as $key => $value)
                     <tr>
