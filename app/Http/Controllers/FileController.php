f33aa8635a08412fb04a7fb1ffb941ee37961fb2
lpfCode <lpf_student@163.com>
2017-11-30 16:40:21 +0800

    sss0sa

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -23,7 +23,7 @@ class FileController extends Controller {
             return response()->json('图片不能为空');
         }else{
 //            $path = "/upload/".'top'.time().'jpg';
-            $path = "/uploads/001.jpeg";
+            $path = "../uploads";
 //            $_FILES['img0']->move($path,$_FILES['img0']['tmp_name']);
             $pathifo = move_uploaded_file($_FILES['img0']['tmp_name'],$path);
             $arr=[
