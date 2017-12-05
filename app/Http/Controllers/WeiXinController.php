590dc10e05a43755e6a7012da61a4ded65e5c7f7
lpfCode <lpf_student@163.com>
2017-12-05 19:36:22 +0800

    sssaaqqw

--- app/Http/Controllers/WeiXinController.php
+++ app/Http/Controllers/WeiXinController.php
@@ -19,36 +19,18 @@ class WeiXinController extends Controller{
 //        $this->middleware('WeiXin');
 //    }
 
-    public function index(){
+    public function index(Request $request){
 
 //        return Input::get('echostr');
 //        $wx = Input::get('echostr');
 ////        return view('wx.index');
 //        return View::make('wx.index')->with('weixinInfo', $wx);
-        $echoStr = $_GET["echostr"];
-        if($this->checkSignature()){
-            echo $echoStr;
-            exit;
-        }
-    }
-    //检查签名
-    private function checkSignature()
-    {
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];
-        $token = "202910";
-        $tmpArr = array($token, $timestamp, $nonce);
-        sort($tmpArr, SORT_STRING);
-        $tmpStr = implode($tmpArr);
-        $tmpStr = sha1($tmpStr);
-        if($tmpStr == $signature){
-            return true;
+        if(empty($_GET["echostr"])){
 
+            return -1;
         }else{
-            return false;
+            return $_GET["echostr"];
         }
-        return View::make('wx.index')->with('weixinInfo', $signature);
     }
 
     public function api(){
