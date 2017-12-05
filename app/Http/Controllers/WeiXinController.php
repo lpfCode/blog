4e806829950fe7c89485100f0a2cd985ad423291
lpfCode <lpf_student@163.com>
2017-12-05 18:22:22 +0800

    sssaaqq

--- app/Http/Controllers/WeiXinController.php
+++ app/Http/Controllers/WeiXinController.php
@@ -14,21 +14,47 @@ use Illuminate\Support\Facades\Input;
 
 class WeiXinController extends Controller{
 
-    public function __construct(){
-
-        $this->middleware('WeiXin');
-    }
+//    public function __construct(){
+//
+//        $this->middleware('WeiXin');
+//    }
 
     public function index(){
 
-        return Input::get('echostr');
-        $wx = Input::get('echostr');
-//        return view('wx.index');
-        return View::make('wx.index')->with('weixinInfo', $wx);
+//        return Input::get('echostr');
+//        $wx = Input::get('echostr');
+////        return view('wx.index');
+//        return View::make('wx.index')->with('weixinInfo', $wx);
+        $echoStr = $_GET["echostr"];
+        if($this->checkSignature()){
+            echo $echoStr;
+            exit;
+        }
     }
+    //检查签名
+    private function checkSignature()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        $token = "202910";
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+        if($tmpStr == $signature){
+            return true;
+
+        }else{
+            return false;
+        }
+        return View::make('wx.index')->with('weixinInfo', $signature);
+    }
+
     public function api(){
 
         echo "ssss000000";
         return view('wx.index');
     }
+
 }
