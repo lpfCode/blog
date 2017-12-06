75dc876482edfff804e1ed5620c90f2941fadbb6
lpfCode <lpf_student@163.com>
2017-12-06 15:04:36 +0800

    awwww

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -8,26 +8,28 @@ use Illuminate\Support\Facades\Input;
 
 class WelcomeController extends Controller{
 
+    public function __construct(Request $request){
+
+            $signature = $request->get('signature');
+            $timestamp = $request->get('timestamp');
+            $nonce = $request->get('nonce');
+            $token = 'weixin';
+            $my_signature = array($token, $timestamp, $nonce);
+            sort($my_signature, SORT_STRING);
+            $my_signature = implode($my_signature);
+            $my_signature = sha1($my_signature);
+            if ($my_signature != $signature) {
+                return -1;
+            } else {
+                return 1;
+            }
+    }
 
     public function index(Request $request){
 
             $data = print_r($_REQUEST, true);
             file_put_contents('/tmp/a.txt', $data . "\n", FILE_APPEND);
-//            $signature = $request->get('signature');
-//            echo "$signature";
-//            $timestamp = $request->get('timestamp');
-//            $nonce = $request->get('nonce');
-//            $token = 'weixin';
-//            $my_signature = array($token, $timestamp, $nonce);
-//            sort($my_signature, SORT_STRING);
-//            $my_signature = implode($my_signature);
-//            $my_signature = sha1($my_signature);
-//            if ($my_signature != $signature) {
-//                return -1;
-//            } else {
-//                return $signature;
-//            }
-        return Input::get('echostr');
+            return Input::get('echostr');
 
 //        return view('welcome');
     }
