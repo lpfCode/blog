f12db7bf0baa9d29b27ca21da3226ce364b5cfe2
lpfCode <lpf_student@163.com>
2017-12-06 14:37:13 +0800

    asdasaqaaa123

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,23 +10,25 @@ class WelcomeController extends Controller{
 
     public function index(Request $request){
 
-            echo "Hello wx";
             $data = print_r($_REQUEST, true);
-            file_put_contents('/tmp/a.txt', $data . "\n", FILE_APPEND);
-            $signature = $request->get('signature');
-            echo "$signature";
-            $timestamp = $request->get('timestamp');
-            $nonce = $request->get('nonce');
-            $token = 'weixin';
-            $my_signature = array($token, $timestamp, $nonce);
-            sort($my_signature, SORT_STRING);
-            $my_signature = implode($my_signature);
-            $my_signature = sha1($my_signature);
-            if ($my_signature != $signature) {
-                return -1;
-            } else {
-                return $signature;
-            }
+//            file_put_contents('/tmp/a.txt', $data . "\n", FILE_APPEND);
+//            $signature = $request->get('signature');
+//            echo "$signature";
+//            $timestamp = $request->get('timestamp');
+//            $nonce = $request->get('nonce');
+//            $token = 'weixin';
+//            $my_signature = array($token, $timestamp, $nonce);
+//            sort($my_signature, SORT_STRING);
+//            $my_signature = implode($my_signature);
+//            $my_signature = sha1($my_signature);
+//            if ($my_signature != $signature) {
+//                return -1;
+//            } else {
+//                return $signature;
+//            }
+        return $data['signature'];
+
+
 
 //        return view('welcome');
     }
