f8304456e3c0c8f977e7ea7e1be58109be9f81bb
lpfCode <lpf_student@163.com>
2017-12-06 16:25:57 +0800

    433

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,7 +6,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 
-define('Token','weixin');
+define('TOKEN','weixin');
 
 class WelcomeController extends Controller{
 
