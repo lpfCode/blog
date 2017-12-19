3e01ae43242305fe396d0fc30b46dda471993b9e
lpfCode <lpf_student@163.com>
2017-12-19 14:35:21 +0800

    连表查询的功能

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -12,10 +12,6 @@ class WelcomeController extends Controller{
 
     public function index(Request $request){
 
-        if($_GET['echostr']) {
-            file_put_contents('/tmp/a.txt', $_GET['echostr'] . "\n", FILE_APPEND);
-            echo $_GET['echostr'];
-        }
         return view("welcome");
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,7 +87,8 @@
                     {{--<a href="https://laracasts.com">Laracasts</a>--}}
                     {{--<a href="https://laravel-news.com">News</a>--}}
                     {{--<a href="https://forge.laravel.com">Forge</a>--}}
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    {{--<a href="https://github.com/laravel/laravel">GitHub</a>--}}
+                    <a href="{{ URL::to('code') }}">算法</a>
                     <a href="{{ URL::to('blog') }}">博客</a>
                     <a href="{{ URL::to('st') }}">数据库基本操作</a>
                     <a href="{{ URL::to('wx') }}">微信平台开发</a>
