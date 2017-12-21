f34a8d4aa918b41c0c55f044f8098187a08ede24
lpfCode <lpf_student@163.com>
2017-12-21 15:03:29 +0800

    ssa

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
-use Illuminate\Support\Facades\Session;
 
 
 class WelcomeController extends Controller{
