9a383a7d7cb43eed1e66ba42fe015f6bc8d5a5bf
lpfCode <lpf_student@163.com>
2017-12-21 15:06:57 +0800

    ssa

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Session;
 
 
 class WelcomeController extends Controller{
 
     public function index(){
+        dd(Session::get['name']);
          if(Session::get['name']!= null){
              return view("welcome");
          }else{//如果不存在跳转到登陆页
